d7b328c70d150ab92d32a93edf42a98b17fc8dc6
nxl5154 <nicholasvilucas@gmail.com>
2023-04-24 16:51:08 -0400

    Initializing project.

---
+++ examples/osrs/brimstoneChest.js
@@ -0,0 +1,119 @@
+import {
+    GetLoot,
+    LootTableEntry,
+} from 'loot-table-advanced'
+import { plot } from 'nodeplotlib'
+  
+function generate_loot() {
+    
+    const drops = [
+        LootTableEntry('mystic_hat', 1),
+        LootTableEntry('mystic_top', 1),
+        LootTableEntry('mystic_bottoms', 1),
+        LootTableEntry('mystic_gloves', 1),
+        LootTableEntry('mystic_boots', 1),
+        LootTableEntry('broken_hasta', 5),
+        LootTableEntry('normies', 990)
+    ]
+
+    function ResolveHelper(id) {
+        switch (id) {
+            case 'drops':
+                return drops;
+        }
+        return null;
+    }
+
+    let mystic_hat = 0
+    let mystic_top = 0
+    let mystic_bottoms = 0
+    let mystic_gloves = 0
+    let mystic_boots = 0
+    let broken_hasta = 0
+    let done = 10000
+    let kc = 0
+
+    for (let i=0; i<done; i++) {
+        let loot = GetLoot(drops, 1, ResolveHelper)
+        let id = loot[0].id
+
+        switch (id) {
+            case 'mystic_hat':
+                mystic_hat = mystic_hat + 1
+                if (mystic_hat === 1) {
+                  //console.log('Mystic hat dusk obtained')
+                }
+                break
+            case 'mystic_top':
+                mystic_top = mystic_top + 1
+                if (mystic_top === 1) {
+                  //console.log('Mystic top dusk obtained')
+                }
+                break
+            case 'mystic_bottoms':
+                mystic_bottoms = mystic_bottoms + 1
+                if (mystic_bottoms === 1) {
+                  //console.log('Mystic bottoms dusk obtained')
+                }
+                break
+            case 'mystic_gloves':
+                mystic_gloves = mystic_gloves + 1
+                if (mystic_gloves === 1) {
+                  //console.log('Mystic gloves dusk obtained')
+                }
+                break
+            case 'mystic_boots':
+                mystic_boots = mystic_boots + 1
+                if (mystic_boots === 1) {
+                  //console.log('Mystic boots dusk obtained')
+                }
+                break
+            case 'broken_hasta':
+                broken_hasta = broken_hasta + 1
+                if (broken_hasta === 1) {
+                  //console.log('Broken dragon hasta obtained')
+                }
+                break
+        }
+        
+        
+        if ( mystic_hat >= 1 && mystic_top >= 1 && mystic_bottoms >= 1 && mystic_boots >= 1 && mystic_gloves >= 1 && broken_hasta >= 1) {
+            //console.log(`KC for completed log: '${i}'`)
+            kc = i
+            return kc
+        }
+
+        if (i === done-1) {
+            //console.log(`Dry log KC: '${done}'`)
+            return done
+        }
+    }
+}
+
+function main() {
+    let total_kc = 0
+    let sims = 10000
+    let avg_kc = 0
+    let kc_list = []
+
+    for (let l=0; l<sims; l++){
+        let temp_kc = generate_loot()
+        total_kc = total_kc + temp_kc
+        kc_list.push(temp_kc)
+    }
+    avg_kc = total_kc/sims
+    let kc_list_sorted = kc_list.sort(function(a,b){return(a-b)})
+
+    const data = [
+        {
+          x: kc_list,
+          type: 'histogram',
+        },
+      ]
+      
+    plot(data)
+
+    console.log(`Average kc to complete log: '${avg_kc}'`)
+}
+
+main()
---
+++ index.js
@@ -0,0 +1,247 @@
+import {
+    GetLoot,
+    LootTableEntry,
+  } from 'loot-table-advanced'
+  
+  function generate_loot() {
+  
+      /*const uniques = [
+          LootTableEntry('head', 1),
+          LootTableEntry('body', 1),
+          LootTableEntry('legs', 1),
+          LootTableEntry('weapon', 1)
+      ]*/
+  
+      const dharok = [
+          LootTableEntry('d_head', 1),
+          LootTableEntry('d_body', 1),
+          LootTableEntry('d_legs', 1),
+          LootTableEntry('d_weapon', 1)
+      ]
+  
+      const guthan = [
+          LootTableEntry('g_head', 1),
+          LootTableEntry('g_body', 1),
+          LootTableEntry('g_legs', 1),
+          LootTableEntry('g_weapon', 1)
+      ]
+  
+      const karil = [
+          LootTableEntry('k_head', 1),
+          LootTableEntry('k_body', 1),
+          LootTableEntry('k_legs', 1),
+          LootTableEntry('k_weapon', 1)
+      ]
+  
+      const ahrim = [
+          LootTableEntry('a_head', 1),
+          LootTableEntry('a_body', 1),
+          LootTableEntry('a_legs', 1),
+          LootTableEntry('a_weapon', 1)
+      ]
+  
+      const torag = [
+          LootTableEntry('t_head', 1),
+          LootTableEntry('t_body', 1),
+          LootTableEntry('t_legs', 1),
+          LootTableEntry('t_weapon', 1)
+      ]
+  
+      const verac = [
+          LootTableEntry('v_head', 1),
+          LootTableEntry('v_body', 1),
+          LootTableEntry('v_legs', 1),
+          LootTableEntry('v_weapon', 1)
+      ]
+  
+      const bros = [
+          LootTableEntry('@dharok', 1),
+          LootTableEntry('@guthan', 1),
+          LootTableEntry('@karil', 1),
+          LootTableEntry('@ahrim', 1),
+          LootTableEntry('@torag', 1),
+          LootTableEntry('@verac', 1)
+      ]
+  
+      const drops = [
+          LootTableEntry('@bros', 1),
+          LootTableEntry('normies', 14)
+      ]
+  
+      function ResolveHelper(id) {
+          switch (id) {
+              case 'dharok':
+                  return dharok;
+              case 'ahrim':
+                  return ahrim;
+              case 'guthan':
+                  return guthan;
+              case 'karil':
+                  return karil;
+              case 'torag':
+                  return torag;
+              case 'verac':
+                  return verac;
+              case 'bros':
+                  return bros;
+              case 'drops':
+                  return drops;
+          }
+          return null;
+      }
+  
+      let d_head = 0
+      let d_body = 0
+      let d_legs = 0
+      let d_weapon = 0
+      let g_head = 0
+      let g_body = 0
+      let g_legs = 0 
+      let g_weapon = 0
+      let k_head = 0
+      let k_body = 0
+      let k_legs = 0
+      let k_weapon = 0
+      let t_head = 0
+      let t_body = 0
+      let t_legs = 0
+      let t_weapon = 0
+      let v_head = 0
+      let v_body = 0
+      let v_legs = 0
+      let v_weapon = 0
+      let a_head = 0
+      let a_body = 0
+      let a_legs = 0
+      let a_weapon = 0
+      let done = 10000
+      let kc = 0
+  
+      for (let i=0; i<done; i++) {
+          let loot = GetLoot(drops, 1, ResolveHelper)
+          let id = loot[0].id
+  
+          switch (id) {
+              case 'd_head':
+                  d_head = d_head + 1
+                  console.log('Dharok helm obtained')
+                  break
+              case 'd_body':
+                  d_body = d_body + 1
+                  console.log('Dharok body obtained')
+                  break
+              case 'd_legs':
+                  d_legs = d_legs + 1
+                  console.log('Dharok legs obtained')
+                  break
+              case 'd_weapon':
+                  d_weapon = d_weapon + 1
+                  console.log
+              case 'g_head':
+                  g_head = g_head + 1
+                  console.log('Guthan helm obtained')
+                  break
+              case 'g_body':
+                  g_body = g_body + 1
+                  console.log('Guthan body obtained')
+                  break
+              case 'g_legs':
+                  g_legs = g_legs + 1
+                  console.log('Guthan legs obtained')
+                  break
+              case 'g_weapon':
+                  g_weapon = g_weapon + 1
+                  console.log('Guthan weapon obtained')
+              case 'k_head':
+                  k_head = k_head + 1
+                  console.log('Karil helm obtained')
+              case 'k_body':
+                  k_body = k_body + 1
+                  console.log('Karil body obtained')
+              case 'k_legs':
+                  k_legs = k_legs + 1
+                  console.log('Karil legs obtained')
+                  break
+              case 'k_weapon':
+                  k_weapon = k_weapon + 1
+                  console.log('Karil weapon obtained')
+                  break
+              case 't_head':
+                  t_head = t_head + 1
+                  console.log('Torag helm obtained')
+                  break
+              case 't_body':
+                  t_body = t_body + 1
+                  console.log('Torag body obtained')
+                  break
+              case 't_legs':
+                  t_legs = t_legs + 1
+                  console.log('Torag legs obtained')
+                  break
+              case 't_weapon':
+                  t_weapon = t_weapon + 1
+                  console.log('Torag weapon obtained')
+                  break
+              case 'v_head':
+                  v_head = v_head + 1
+                  console.log('Verac helm obtained')
+                  break
+              case 'v_body':
+                  v_body = v_body + 1
+                  console.log('Verac body obtained')
+                  break
+              case 'v_legs':
+                  v_legs = v_legs + 1
+                  console.log('Verac legs obtained')
+                  break
+              case 'v_weapon':
+                  v_weapon = v_weapon + 1
+                  console.log('Verac weapon obtained')
+                  break
+              case 'a_head':
+                  a_head = a_head + 1
+                  console.log('Ahrim helm obtained')
+                  break
+              case 'a_body':
+                  a_body = a_body + 1
+                  console.log('Ahrim body obtained')
+                  break
+              case 'a_legs':
+                  a_legs = a_legs + 1
+                  console.log('Ahrim legs obtained')
+                  break
+              case 'a_weapon':
+                  a_weapon = a_weapon + 1
+                  console.log('Ahrim weapon obtained')
+                  break
+          }
+          
+          
+          if (d_head >= 1 && d_body >= 1 && d_legs >= 1 && d_weapon >=1 && g_head >= 1 && g_body >= 1 && g_legs >= 1 && g_weapon >= 1 && k_head >= 1 && 
+          k_body >= 1 && k_legs >= 1 && k_weapon >= 1 && t_head >= 1 && t_body >= 1 && t_legs >= 1 && t_weapon >= 1 && v_head >= 1 && v_body >= 1 && 
+          v_legs >= 1 && v_weapon >= 1 && a_head >= 1 && a_body >= 1 && a_legs >= 1 && a_weapon >= 1) {
+              console.log(`KC for completed log: '${i}'`)
+              kc = i
+              return kc
+          }
+  
+          if (i === done-1) {
+              console.log(`Dry log KC: '${done}'`)
+              return done
+          }
+      }
+  }
+  
+  function main() {
+      let total_kc = 0
+      let sims = 10000
+      let avg_kc = 0
+      for (let l=0; l<sims; l++){
+          total_kc = total_kc + generate_loot() 
+      }
+      avg_kc = total_kc/sims
+      console.log(`Average kc to complete log: '${avg_kc}'`)
+  }
+  
+  main()
+  
